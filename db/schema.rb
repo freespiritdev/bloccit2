0f9e6c40cd64b9b82eaa9c781ba2f52b4bb21410
avyishi <avyishi@gmail.com>
2014-11-01 02:47:43 -0700

    Associated and nested topics and posts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101052007) do
+ActiveRecord::Schema.define(version: 20141101063254) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -25,6 +25,19 @@ ActiveRecord::Schema.define(version: 20141101052007) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "topic_id"
+  end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "topics", force: true do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -47,6 +60,7 @@ ActiveRecord::Schema.define(version: 20141101052007) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
